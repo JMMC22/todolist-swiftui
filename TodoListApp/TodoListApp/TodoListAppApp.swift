412f1abcb51145912df939cabf8f075634ad843f
José María <jmmarquezcrepo@gmail.com>
2022-11-11 16:25:00 +0100

    Initial project

---
+++ TodoListApp/TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Jose Mari on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
